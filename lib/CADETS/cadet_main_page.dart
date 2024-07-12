195a474ab15c421f065fbb1c1ae7a859acf46757
deepak-dee3 <deepakdscute@gmail.com>
2024-07-12 11:55:42 +0530

    new dart file has been added for cadets

--- lib/CADETS/cadet_main_page.dart
+++ lib/CADETS/cadet_main_page.dart
@@ -1,33 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:ncc/ANO/ano_view_camp_events.dart';
-import 'package:ncc/ANO/dumy_cadet_details.dart';
-import 'package:ncc/ANO/cadets_all_details.dart.dart';
-import 'package:ncc/ANO/cadets_list.dart';
-import 'package:ncc/ANO/upload_camps.dart';
-import 'package:ncc/ANO/upload_campss.dart';
 
+class cadet_main_page extends StatefulWidget{
+  @override
+  State<cadet_main_page> createState() => _cadet_main_pageState();
+}
 
-class ano_view_details extends StatelessWidget
-{
+class _cadet_main_pageState extends State<cadet_main_page> {
   @override
   Widget build(BuildContext context) {
-    
-    return 
-    WillPopScope(
-      onWillPop: () async {
-        // Handle back button press
-        Navigator.pop(context);
-        return true;
-      },
-      child:Scaffold(
-
+   
+   return(
+    Scaffold(
       body:SingleChildScrollView(scrollDirection: Axis.vertical,
         child:Container(
         child:Column(children: [
 
           SizedBox(height: 70,),
          GestureDetector(onTap:(){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => ccadet_details()));
+         // Navigator.push(context, MaterialPageRoute(builder: (context) => ccadet_details()));
          },
           child:Padding(padding: EdgeInsets.only(left:30,top:60),
           child:Container(
@@ -36,27 +27,12 @@ class ano_view_details extends StatelessWidget
             decoration: BoxDecoration(color: Color.fromARGB(255, 39, 3, 116),borderRadius: BorderRadius.circular(20)),
             child: Row(children: [
 
-            Text('    CADET\'S DETAILS                         ',style:TextStyle(color:Colors.blue,fontWeight: FontWeight.bold)),
+            Text('    PROFILE UPDATION                         ',style:TextStyle(color:Colors.blue,fontWeight: FontWeight.bold)),
             Icon(Icons.arrow_circle_right_outlined,color:Colors.blue)
           ],),)),),
 
-          
 
-          GestureDetector(onTap:(){
-            Navigator.push(context,MaterialPageRoute(builder: (context) => upload_camp_detailss()));
-          },
-            child:Padding(padding: EdgeInsets.only(left:30,top:60),
-          child:Container(
-            width:330,
-            height: 100,
-            decoration: BoxDecoration(color: Color.fromARGB(255, 39, 3, 116),borderRadius: BorderRadius.circular(20)),
-            child: Row(children: [
-
-            Text(' UPLOAD CAMP / EVENT DETAILS  ',style:TextStyle(fontWeight: FontWeight.bold,color:Colors.blue)),
-            Icon(Icons.arrow_circle_right_outlined,color: Colors.blue,)
-          ],),)),),
-
-          GestureDetector(onTap:(){
+           GestureDetector(onTap:(){
           Navigator.push(context, MaterialPageRoute(builder: (context) => ano_camp_view_details()));
          },
           child:Padding(padding: EdgeInsets.only(left:30,top:60),
@@ -70,8 +46,26 @@ class ano_view_details extends StatelessWidget
             Icon(Icons.arrow_circle_right_outlined,color:Colors.blue)
           ],),)),),
 
+          
+
           GestureDetector(onTap:(){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => ano_camp_view_details()));
+           // Navigator.push(context,MaterialPageRoute(builder: (context) => upload_camp_detailss()));
+          },
+            child:Padding(padding: EdgeInsets.only(left:30,top:60),
+          child:Container(
+            width:330,
+            height: 100,
+            decoration: BoxDecoration(color: Color.fromARGB(255, 39, 3, 116),borderRadius: BorderRadius.circular(20)),
+            child: Row(children: [
+
+            Text(' UPLOAD ACHIEVEMENTS  ',style:TextStyle(fontWeight: FontWeight.bold,color:Colors.blue)),
+            Icon(Icons.arrow_circle_right_outlined,color: Colors.blue,)
+          ],),)),),
+
+         
+
+          GestureDetector(onTap:(){
+         // Navigator.push(context, MaterialPageRoute(builder: (context) => ano_camp_view_details()));
          },
           child:Padding(padding: EdgeInsets.only(left:30,top:60),
           child:Container(
@@ -113,7 +107,13 @@ class ano_view_details extends StatelessWidget
       )
 
 
-    )));
-  }
+    ))
 
+      
+
+      
+
+    
+   );
+  }
 }
